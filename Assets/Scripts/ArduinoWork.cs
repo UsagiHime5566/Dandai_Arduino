53337a5628a5180b583f8814d588abc90ca95806
nikki <cuapopomama2@gmail.com>
2021-02-21 00:14:57 +0800

    整合 Arduino

---
+++ Assets/Scripts/ArduinoWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArduinoWork : MonoBehaviour
+{
+    public string Msg_Recv_Ready;
+    public string Msg_Recv_ToRun;
+    public string Msg_Send_ToJump;
+    public string Msg_Send_ToBack;
+    public ArduinoInteractive arduinoInteractive;
+
+    public Action OnStageReady;
+    public Action OnAnimStartRun;
+    
+    void Start()
+    {
+        arduinoInteractive.OnRecieveData += RecievedSignals;
+    }
+
+    
+    // 爆破音效
+    void SendJumpMsg(){
+        arduinoInteractive.SendData(Msg_Send_ToJump);
+    }
+
+    // 黑幕降臨, 提示裝置該歸位了
+    void SendBack(){
+        arduinoInteractive.SendData(Msg_Send_ToBack);
+    }
+
+    void RecievedSignals(string msg){
+        // 開始你的表演
+        if(msg == Msg_Recv_ToRun){
+            OnAnimStartRun?.Invoke();
+        }
+
+        // 舞台準備就緒, 黑幕可以拉開了
+        if(msg == Msg_Recv_Ready){
+            OnStageReady?.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/UniArtpower/Module/SystemConfigExtend.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class SystemConfigExtend
+{
+    public static void SetLabels(this Dropdown dd, string [] aName)
+    {
+        dd.options.Clear();
+        foreach (var item in aName)
+        {
+            Dropdown.OptionData data = new Dropdown.OptionData(){
+                text = item
+            };
+            dd.options.Add(data);
+        }
+    }
+
+    public static int SetSavedData(this Dropdown dd, string savedString, Action<int> changedToDo){
+        int id = SystemConfig.Instance.GetData<int>(savedString);
+        dd.value = id;
+        dd.captionText.text = dd.options[dd.value].text;
+
+        dd.onValueChanged.AddListener(x => {
+            SystemConfig.Instance.SaveData(savedString, x);
+            changedToDo?.Invoke(x);
+        });
+
+        //如果 id 大於 DropValue, 他會自己限縮, 因此要讀取 DropValue
+        int resultId = dd.value;
+        dd.onValueChanged.Invoke(resultId);
+
+        return resultId;
+    }
+
+}
